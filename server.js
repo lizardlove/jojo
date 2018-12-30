b372074a7858dd5ca7a8de1105a125087c6b5aec
LizardLove <1026123335@qq.com>
2018-12-30 20:43:06 +0800

    charset

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 8888;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/plain;charset=utf-8');
   res.end('JOJO, 我不做人了\n');
 });
 
